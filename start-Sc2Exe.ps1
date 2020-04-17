c0ed71671752dc75f1901bfde82d1c3ad4c9308d
Michael Eselgroth <meselgroth@yahoo.com>
2020-04-17 18:07:02 +1000

    Powershell script to open starcraft in windowed mode with websocket open

---
+++ start-Sc2Exe.ps1
@@ -0,0 +1,4 @@
+# open starcraft in windowed mode with websocket open
+pushd 'C:\Program Files (x86)\StarCraft II\Support64\'   
+& 'C:\Program Files (x86)\StarCraft II\Versions\Base78285\SC2_x64.exe' -listen 127.0.0.1 -port 5678 -displayMode 0
+popd
