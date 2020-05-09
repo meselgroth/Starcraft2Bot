8b8853612c0eea92710e1e1023cabe179a21c8a0
Michael Eselgroth <meselgroth@yahoo.com>
2020-05-09 14:41:45 +1000

    Add frontend using create-react-app

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HiveMind
+      </header>
+      <p>Start</p>
+    </div>
+  );
+}
+
+export default App;
